5fe21b744b4fd991ce1a74f6af70f681e5b8601a
cristi9512 <nicacristi20@gmail.com>
2020-01-11 16:06:02 +0200

    show elements from collection

--- index.js
+++ index.js
@@ -3,21 +3,40 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const MongoClient = require('mongodb').MongoClient;
 
-express()
+const app = express();
+let db = null;
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/bogdan', (req, res) => {
-    res.send('Salut ba chesche');
+    //res.send('Salut ba chesche');
+	db.collection("positions").find({}).toArray(function (err, result) {
+		    if (err) {
+				//console.log('error');
+		        //res.send(err);
+		    } else {
+				res.send(JSON.stringify({message: "Ce cauti aici?"}));
+		        //res.send(JSON.stringify(result));
+				console.log(JSON.stringify(result));
+		    }
+		})
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+	.get('/allPositions', (req, res) => {
+		db.collection("positions").find({}).toArray(function (err, result) {
+				if (err) {
+					console.log('error');
+				    res.send(err);
+				} else {
+				    res.send(JSON.stringify(result));
+					console.log(JSON.stringify(result));
+				}
+			})
+	  });
+  
 
-
-/*const app = express();
-app.get('/bogdan', (req, res) => {
-    res.send('Salut ba chesche');
-});
 
 
 
@@ -27,11 +46,19 @@ MongoClient.connect(uri, function(err, client) {
    if(err) {
         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
    }
-   console.log('Connected...');
-   const collection = client.db("bd-project").collection("position");
-   console.log(collection);
+   	console.log('Connected...');
+	db = client.db("bd-project");
+	app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+   /*const collection = client.db("bd-project").collection("positions").find({}).toArray(function (err, result) {
+        if (err) {
+			console.log('error');
+            //res.send(err);
+        } else {
+            //res.send(JSON.stringify(result));
+			console.log(JSON.stringify(result));
+        }
+    })
+  
    // perform actions on the collection object
-   client.close();
+   client.close();*/
 })
-
-*/
